7be6654174558f4bd8a7bbd831001aeb42f31dc6
Rizwan Alam <v-rizwan.alam@yatra.com>
2023-04-05 14:06:01 +0530

    first commit of frontend of mannat gift house

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/common/header";
+
+function App() {
+  return (
+    <div className="home_page_wrapper">
+      <h3 className="text-center">MANNAT GIFT HOUSE</h3>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
