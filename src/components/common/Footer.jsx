fe8514723e3b328797d2eda0d23992fcf44c09b1
Rizwan Alam <v-rizwan.alam@yatra.com>
2023-09-26 16:18:00 +0530

    color theme changed

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import mgLogo from "../../assets/icons/mg-logo2.png";
+import mgLogo from "../../assets/icons/mannat_logo.png";
 import instaLogo from "../../assets/icons/insta.svg";
 import ytLogo from "../../assets/icons/yt.svg";
 import fb from "../../assets/icons/fb.svg";
